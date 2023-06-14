cfa8c9ac2d9d40ded5682ee808e307d11196fd51
Abel Palavecino <abel@big.parters>
2023-06-13 21:16:32 -0300

    normalidad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,13 @@ export default async function Home() {
   const links = await api.links.fetch();
   
   return <main> 
-          
+          <h1>Bajadas</h1>
+          <ul>
+            {links.map((link) => (
+              <li key={link.url}>
+                <a href={link.url}>{link.label}</a>
+              </li>
+            ))}
+          </ul>
         </main>
 }
