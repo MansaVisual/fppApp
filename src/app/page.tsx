099b0146b5a742f1ae7f19ef095b09b6d0629809
Abel Palavecino <abel@big.parters>
2023-07-04 12:58:37 -0300

    esqueleto de columnas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
             <header>
               <nav className="site-nav container">
                 <div className="row">
-                  <div className="col-36-5">
+                  <div className="col-36-5 offset-left-36-2">
                     <div className="logo">
                       <figure>
                         <img src="" alt="" />
@@ -20,7 +20,7 @@ export default async function Home() {
                     </div>
                   </div>
 
-                  <div className="col-36-28">
+                  <div className="col-36-28 offset-left-36-1">
                     <ul className="main-menu">
                       <li>
                         <a href="#">volver</a>
@@ -35,39 +35,56 @@ export default async function Home() {
                 </div>
               </nav>
             </header>
-          {/* end header  */}
+          {/* header  */}
+
+          {/* image header */}
+          <section className="image-header container">
+            <div className="row">
+              <div className="col-36-34 offset-left-36-1">
+                <figure>
+                  <img src="#" alt="#" />
+                </figure>
+              </div>
+            </div>
+          </section>
+          {/* image header */}
 
           {/* main */}
-            <main className="content-main"> 
-              <figure>
-                <img src="#" alt="#" />
-              </figure>
-              <ul className="dowloads-menu">
-                {links.map((link) => (
-                  <li key={link.url}>
-                    <a href={link.url}>{link.label}</a>
-                  </li>
-                ))}
-              </ul>
+            <main className="content-main container">
+              <div className="row">
+                <div className="col-36-34 offset-left-36-1"> 
+                  <ul className="dowloads-menu">
+                    {links.map((link) => (
+                      <li key={link.url}>
+                        <a href={link.url}>{link.label}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </main>
-          {/* end main */}
+          {/* main */}
           
           {/* footer */}
-            <footer>
-              <div className="row-container">
-                <aside className="partners"></aside>
-                <ul className="social-media">
-                  <li>
-                    <a href=""></a>
-                  </li>
-                </ul>
-                <address>
-                  <p>Av. del Libertador 8620, C1429 - C.A.B.A. - ARGENTINA</p>
-                  <p>TELÉFONO (+54) 11 5591-1077 //  SECRETARIAFPP@GMAIL.COM</p>
-                </address>
+            <footer className="container site-footer">
+              <div className="row">
+                <aside className="col-36-34 offset-left-36-1 partners">
+                  {/* aca van los logos */}
+                </aside>
+                <section className="info col-36-14 offset-left-36-11">
+                  <ul className="social-media">
+                    <li>
+                      <a href=""></a>
+                    </li>
+                  </ul>
+                  <address>
+                    <p>Av. del Libertador 8620, C1429 - C.A.B.A. - ARGENTINA</p>
+                    <p>TELÉFONO (+54) 11 5591-1077 //  SECRETARIAFPP@GMAIL.COM</p>
+                  </address>
+                </section>
               </div>
             </footer>
-          {/* end footer */}
+          {/* footer */}
 
         </div> 
         // @end wrapper
